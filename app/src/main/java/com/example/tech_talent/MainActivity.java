f7ccc53aeebe762803f411a103ed1824ac30bf40
Mateus Padilha <mateuspmota2010@gmail.com>
2022-09-20 22:06:16 -0300

    Starting to implement the home screen

--- app/src/main/java/com/example/tech_talent/MainActivity.java
+++ app/src/main/java/com/example/tech_talent/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.tech_talent;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.cardview.widget.CardView;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    CardView cardList;
+    CardView cardEdit;
+    CardView cardRegister;
+    CardView cardDelete;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cardList = findViewById(R.id.cardProjectList);
+        cardEdit = findViewById(R.id.cardEditProject);
+        cardRegister = findViewById(R.id.cardRegisterProject);
+        cardDelete = findViewById(R.id.cardDeleteProject);
+
+        cardList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        cardEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        cardRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        cardDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
